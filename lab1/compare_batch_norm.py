72f4f0e549c962ae3681fb8d98474c73d9f225f7
Matteo Rosati <matteo.rosati@yale.edu>
2021-11-15 18:24:06 +0100

    Working implementation of batch norm with plots

--- lab1/compare_batch_norm.py
+++ lab1/compare_batch_norm.py
@@ -25,8 +25,9 @@ import numpy as np
 import os
 from mlp_pytorch import MLP
 import cifar10_utils
-import train_mlp_pytorch
+import train_mlp_pytorch as mlp
 import pickle
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
@@ -54,8 +55,44 @@ def train_models(results_filename):
     # PUT YOUR CODE HERE  #
     #######################
     # TODO: Run all hyperparameter configurations as requested
-    results = None
+    results = []
+    lr = 0.1
+    batch_size = 128
+    epochs = 20
+    seed = 42
+    data_dir = "data/"
+    hidden_dims_array = [[128], [256, 128], [512, 256, 128]]
+
+    print("Training all models")
+    for i, hidden_dims in enumerate(hidden_dims_array):
+        print("Without BatchNorm")
+        model, val_accuracies, test_accuracy, logging_dict = mlp.train(
+            hidden_dims, lr, False, batch_size, epochs, seed, data_dir
+        )
+        results_nobatch = {
+            "info": model.info,
+            "val_acc": val_accuracies,
+            "test_acc": test_accuracy,
+            "other": logging_dict,
+        }
+
+        print("With BatchNorm")
+        model, val_accuracies, test_accuracy, logging_dict = mlp.train(
+            hidden_dims, lr, True, batch_size, epochs, seed, data_dir
+        )
+        results_batch = {
+            "info": model.info,
+            "val_acc": val_accuracies,
+            "test_acc": test_accuracy,
+            "other": logging_dict,
+        }
+        results += [results_nobatch, results_batch]
+
     # TODO: Save all results in a file with the name 'results_filename'. This can e.g. by a json file
+
+    with open(results_filename, "wb") as handle:
+        pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
     #######################
     # END OF YOUR CODE    #
     #######################
@@ -77,7 +114,47 @@ def plot_results(results_filename):
     #######################
     # PUT YOUR CODE HERE  #
     #######################
-    pass
+    with open(results_filename, "rb") as handle:
+        data = pickle.load(handle)
+
+    f, axs = plt.subplots(2, 3, sharey="row")
+    f.suptitle("Hyperparameter Configurations Results")
+    plt.subplots_adjust(hspace=0.5, left=0.05, right=0.95)
+    for j in range(0, len(data), 2):
+        i = j // 2
+        epochs = data[0]["other"]["epochs"]
+        axs[0, i].plot(
+            epochs, data[j]["other"]["train_acc"], "b-", label="No batch norm",
+        )
+        axs[0, i].plot(
+            epochs, data[j + 1]["other"]["train_acc"], "b--", label="With batch norm",
+        )
+
+        axs[1, i].plot(
+            epochs, data[j]["val_acc"], "r-", label="No batch norm",
+        )
+        axs[1, i].plot(
+            epochs, data[j + 1]["val_acc"], "r--", label="With batch norm",
+        )
+
+        n_hidden = data[2 * i]["info"]["hidden_layers"]
+
+        axs[0, i].set_title(
+            "Training Accuracies per Epoch\n" + f"for {n_hidden} hidden dimensions"
+        )
+        axs[1, i].set_title(
+            "Validation Accuracies per Epoch\n" + f"for {n_hidden} hidden dimensions"
+        )
+        axs[0, i].set_xlabel("Epochs")
+        axs[0, i].set_ylabel("Accuracy")
+        axs[1, i].set_xlabel("Epochs")
+        axs[1, i].set_ylabel("Accuracy")
+
+    axs[0, 2].legend(loc="lower right")
+    axs[1, 2].legend(loc="lower right")
+    # f.tight_layout(pad=0.2)
+    plt.show()
+
     #######################
     # END OF YOUR CODE    #
     #######################
@@ -85,7 +162,7 @@ def plot_results(results_filename):
 
 if __name__ == "__main__":
     # Feel free to change the code below as you need it.
-    FILENAME = "results.txt"
+    FILENAME = "results_batchnorm.txt"
     if not os.path.isfile(FILENAME):
         train_models(FILENAME)
     plot_results(FILENAME)
