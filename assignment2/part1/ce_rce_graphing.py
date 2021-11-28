ff425f01937c2e8ba2b0a357863989a8924f45f3
Matteo Rosati <matteo.rosati@yale.edu>
2021-11-28 20:01:34 +0100

    Implemented results data loading

--- assignment2/part1/ce_rce_graphing.py
+++ assignment2/part1/ce_rce_graphing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import argparse
+import json
 
 
 def ce(accuracies, resnet18_acc):
@@ -13,4 +14,23 @@ def rce(accuracies, resnet18_acc):
 
 if __name__ == "__main__":
     models = ["vgg11", "vgg11_bn", "resnet18", "resnet34", "densenet121"]
+
+    model_results = os.listdir("results/")
+    accuracies = {}
+
+    # loop over results files and load into memory
+    for m in model_results:
+        path = os.path.join("results", m)
+        model_name = m[: m.rfind("_")]
+        print(f"loading {model_name} data")
+        with open(path, "r") as f:
+            accuracies[model_name] = json.load(f)
+    print(accuracies)
+
+    # calculate ce and rce
+
+    # plot results
+    # resnet18 plot
+
+    # other model plots
 
