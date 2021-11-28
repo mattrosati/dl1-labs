04406f4eb13197e61a7f2c1d37086be107c1f13f
Matteo Rosati <matteo.rosati@yale.edu>
2021-11-28 11:17:53 +0100

    Starting plotting script

---
+++ assignment2/part1/ce_rce_graphing.py
@@ -0,0 +1,12 @@
+import numpy as np
+import os
+import argparse
+
+
+def ce(accuracies, resnet18_accuracies):
+    pass
+
+
+if __name__ == "__main__":
+    models = ["debug", "vgg11", "vgg11_bn", "resnet18", "resnet34", "densenet121"]
+
