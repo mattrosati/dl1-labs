d4d70e59a31671288abfee1f989985fa3e0a83fa
Matteo Rosati <matteo.rosati@yale.edu>
2021-11-29 01:29:12 +0100

    Added resnet18 accuracy plotting

--- assignment2/part1/ce_rce_graphing.py
+++ assignment2/part1/ce_rce_graphing.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import argparse
 import json
+import matplotlib.pyplot as plt
 
 
 def ce(accuracies, resnet18_acc):
@@ -14,6 +15,14 @@ def rce(accuracies, resnet18_acc):
 
 if __name__ == "__main__":
     models = ["vgg11", "vgg11_bn", "resnet18", "resnet34", "densenet121"]
+    corruptions = {
+        "clean": "Baseline",
+        "gnoise": "Gaussian Noise",
+        "gblur": "Gaussian Blur",
+        "contrast": "Contrast Reduction",
+        "jpg": "JPEG Compression",
+    }
+    colors = ["green", "cyan", "magenta", "purple"]
 
     model_results = os.listdir("results/")
     accuracies = {}
@@ -25,12 +34,30 @@ if __name__ == "__main__":
         print(f"loading {model_name} data")
         with open(path, "r") as f:
             accuracies[model_name] = json.load(f)
-    print(accuracies)
 
-    # calculate ce and rce
+    # calculate ce and rce, plot resnet18 results
+    metrics = {}
+    metrics["ce"] = {}
+    metrics["rce"] = {}
+    for val in accuracies.keys():
+        if val != "resnet18":
+            ce = tbd
+        else:
+            f, ax = plt.subplots(1, 1)
+            for c in accuracies[val].keys():
+                if c == "clean":
+                    ax.plot(
+                        accuracies[val][c], np.arange(1, 6), "k--", label=corruptions[c]
+                    )
+                else:
+                    ax.plot(
+                        accuracies[val][c],
+                        np.arange(1, 6),
+                        color=next(iter(colors)),
+                        linestyle="solid",
+                        label=corruptions[c],
+                    )
+            plt.show()
 
-    # plot results
-    # resnet18 plot
-
-    # other model plots
+    # plot rce and ce results
 
