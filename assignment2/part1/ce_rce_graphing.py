81f086b56d77ec9a008b963aafd681880f242828
Matteo Rosati <matteo.rosati@yale.edu>
2021-11-29 11:07:01 +0100

    Completed ResNet18 accuracy graphing

--- assignment2/part1/ce_rce_graphing.py
+++ assignment2/part1/ce_rce_graphing.py
@@ -5,6 +5,20 @@ import json
 import matplotlib.pyplot as plt
 
 
+def load_results(file_list):
+    accuracies = {}
+
+    # loop over file list and load into memory
+    for m in file_list:
+        path = os.path.join("results", m)
+        model_name = m[: m.rfind("_")]
+        print(f"loading {model_name} data")
+        with open(path, "r") as f:
+            accuracies[model_name] = json.load(f)
+
+    return accuracies
+
+
 def ce(accuracies, resnet18_acc):
     pass
 
@@ -22,41 +36,45 @@ if __name__ == "__main__":
         "contrast": "Contrast Reduction",
         "jpg": "JPEG Compression",
     }
-    colors = ["green", "cyan", "magenta", "purple"]
+    colors = ["green", "blue", "red", "purple"]
 
     model_results = os.listdir("results/")
-    accuracies = {}
-
-    # loop over results files and load into memory
-    for m in model_results:
-        path = os.path.join("results", m)
-        model_name = m[: m.rfind("_")]
-        print(f"loading {model_name} data")
-        with open(path, "r") as f:
-            accuracies[model_name] = json.load(f)
+    accuracies = load_results(model_results)
 
     # calculate ce and rce, plot resnet18 results
     metrics = {}
     metrics["ce"] = {}
     metrics["rce"] = {}
-    for val in accuracies.keys():
-        if val != "resnet18":
-            ce = tbd
+    for key in accuracies.keys():
+        if key != "resnet18":
+            metrics["ce"][key] = ce(accuracies[key], accuracies["resnet18"])
+            metrics["rce"][key] = rce(accuracies[key], accuracies["resnet18"])
         else:
             f, ax = plt.subplots(1, 1)
-            for c in accuracies[val].keys():
+            for c in accuracies[key].keys():
                 if c == "clean":
                     ax.plot(
-                        accuracies[val][c], np.arange(1, 6), "k--", label=corruptions[c]
+                        np.arange(1, 6),
+                        np.repeat(accuracies[key][c], 5),
+                        "k--",
+                        label=corruptions[c],
                     )
                 else:
                     ax.plot(
-                        accuracies[val][c],
                         np.arange(1, 6),
-                        color=next(iter(colors)),
+                        accuracies[key][c],
+                        color=colors.pop(),
+                        marker="o",
                         linestyle="solid",
                         label=corruptions[c],
                     )
+            ax.set_xlabel("Corruption Severity")
+            ax.set_ylabel("Top-1 Accuracy")
+            ax.set_title(
+                "Test Accuracy of ResNet18 with various corruption functions (CIFAR10 Dataset)"
+            )
+            ax.set_xticks(np.arange(1, 6))
+            ax.legend()
             plt.show()
 
     # plot rce and ce results
